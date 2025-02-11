a046fa2e861bb9dd7a253343692558c61bb50d64
Micha R. Okun <mokun@slac.stanford.edu>
2025-02-11 14:41:17 -0800

    point incidents and services hooks at slaclab/s3df-status-logs; update this repo config next.config.js and image loc to this slaclab/s3df-status-display; disable Actions based health checks

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@
 const production = process.env.NODE_ENV === "production";
 
 const nextConfig = {
-  assetPrefix: production ? '/s3df-status' : '',
+  assetPrefix: production ? '/s3df-status-display' : '',
   reactStrictMode: true,
   swcMinify: true,
 }
