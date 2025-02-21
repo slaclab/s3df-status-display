fad526e5b1f0840c7031d4d35ef2bb2a2be9cf93
ac6y micha <ac6y@users.noreply.github.com>
2025-02-21 09:51:02 -0800

    Revert "Use locale string for last updated"

--- src/services/index.tsx
+++ src/services/index.tsx
@@ -43,7 +43,7 @@ const ServicesSection: NextPage = () => {
                     </div>
                     <div>
                         <p className="text-xs text-gray-400">Last updated</p>
-                        <p className="text-xs text-gray-400 text-end ">{new Date(systemStatus?.datetime).toLocaleString()}</p>
+                        <p className="text-xs text-gray-400 text-end ">{systemStatus?.datetime}</p>
                     </div>
                 </div>
             </div>
